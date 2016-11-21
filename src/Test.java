a93a6fcc68d4fa834e12f61a6f2cee79e75940b5
vasivkov <sivkov_va@mail.ru>
2016-11-21 11:53:40 +0300

    renamed

--- src/Test.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Василий on 21.11.2016.
- */
-public class Test {
-}
---
+++ src/com/mycompany/lessons/Test.java
@@ -0,0 +1,10 @@
+package com.mycompany.lessons;
+
+/**
+ * Created by Василий on 21.11.2016.
+ */
+public class Test {
+    public static void main(String[] args) {
+        
+    }
+}
