bc95110d1c1f6bff529ca75320b7a0535e338af5
vasivkov <sivkov_va@mail.ru>
2016-12-05 11:56:38 +0300

    delete out and .idea

--- src/com/mycompany/horman/Manager.java
+++ src/com/mycompany/horman/Manager.java
@@ -3,5 +3,29 @@ package com.mycompany.horman;
 /**
  * Created by Василий on 01.12.2016.
  */
- class Manager extends Employee {
+public class Manager extends Employee {
+    /**
+     @param n имя сотрудника
+     @param s Зарплата
+     @param year год найма
+     @param month месяц
+     @param day день
+     */
+
+    public Manager (String n, double s, int year, int month, int day){
+        super(n, s, year, month, day);
+        bonus = 0;
+    }
+
+    @Override
+    public double getSalary() {
+        double baseSalary = super.getSalary();
+        return  baseSalary + bonus;
+    }
+
+    public  void setBonus (double b){
+        bonus = b;
+    }
+
+    private  double bonus;
 }
